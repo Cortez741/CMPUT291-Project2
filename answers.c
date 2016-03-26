8bfb4cefde54b28381c06f9b315c7b2068bdb880
Victor Frunza <victor.f@outlook.com>
2016-03-26 12:59:27 -0600

    Sync

---
+++ answers.c
@@ -0,0 +1,18 @@
+//---answers.c----------------------------------------------------------------------------------------------------------------------------
+// Authors: Victor Frunza & Ian Oltuszyk
+// Instructor: Li-Yan Yuan
+// TA: Kriti Khare
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//when using this function, make sure to append "" to both ends of the string before passing the string into this function.
+int main(int argc, char * argv[]){
+    //opens the file
+    FILE * answers = fopen("answers.txt", "a+");
+    //prints the input to the file
+    fprintf(answers, "%s\n", argv[1]);
+    //closes the file
+    fclose(answers);
+    return 0;
+}
