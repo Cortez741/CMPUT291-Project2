7b38313ffb1321710a2b7f1b09e49ef513f9ba0a
Ian Oltuszyk <ian.oltu@gmail.com>
2016-04-03 13:20:16 -0600

    BTree Progress

--- CMPUT291-Project2/btree.h
+++ CMPUT291-Project2/btree.h
@@ -7,10 +7,16 @@
 typedef struct BTreeDatabase BTD;
 struct BTreeDatabase {
 	DB* db; 
+	void(*create)(BTD*);
 	void(*destroy)(BTD*);
+	void(*populate)(BTD*);
+	int(*menu)(BTD*);
 };
 void BTD_init(BTD* self);
+void BTD_create(BTD* self);
 void BTD_destroy(BTD* self);
+void BTD_populate(BTD* self);
+int BTD_menu(BTD* self);
 void BTreeDB();
 
 #endif
